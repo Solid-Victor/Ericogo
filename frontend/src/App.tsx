02d2d145d53db44695ba3351ef18526d193d9f8b
Solid-Victor <victorcontents@gmail.com>
2024-07-17 00:58:15 +0100

    list of products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import { sampleProducts } from './data'
+import './App.css'
 
 function App() {
   return (
     <div>
       <header>TS Amazona</header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => {
+           return <li key={product.slug}>
+            <img src={product.image} alt={product.image} className='product-image' />
+             <h2>{product.name}</h2> 
+            </li>
+          })}
+        </ul>
+      </main>
       <footer>All right reserved</footer>
     </div>
   )
