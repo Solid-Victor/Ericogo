f27671bbfff3c0dfb07fbc8817bdac274a593650
Solid-Victor <victorcontents@gmail.com>
2024-07-16 22:19:40 +0100

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <header>TS Amazona</header>
+      <main></main>
+      <footer>All right reserved</footer>
+    </div>
+  )
+}
+
+export default App
